abeff254a1d871d88e36916051a5890db2e009aa
saibaanwar77@gmail.com <saibaanwar77@gmail.com>
2026-05-27 01:59:26 +0300

    Fix: database connection pool limit

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,6 +2,9 @@ import { Pool } from 'pg'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  max: 1,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000,
 })
 
 export default pool
